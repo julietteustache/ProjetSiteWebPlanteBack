a6c6e7bd0ef85e2cf22c361b3d4466c8694f1552
Rose-MarieG <rosemarie.gallon@gmail.com>
2021-10-29 10:16:16 +0200

    Contrainte clé pour commande

---
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/entities/AnnonceUserConstraint.java
@@ -0,0 +1,14 @@
+package fr.solutec.entities;
+
+import java.io.Serializable;
+
+public class AnnonceUserConstraint implements Serializable{
+	
+	private Long annonce;
+	
+	private Long user;
+	
+	public AnnonceUserConstraint() {
+//création du couple de clé primaire		
+	}
+}
--- ProjetSiteWebPlante/src/main/java/fr/solutec/entities/Commande.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/entities/Commande.java
@@ -1,6 +1,10 @@
 package fr.solutec.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
@@ -9,11 +13,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@IdClass(AnnonceUserConstraint.class)
 @NoArgsConstructor @AllArgsConstructor @Data
-public class Commande {
+public class Commande implements Serializable{
+	
+	@Id
 	@ManyToOne
 	private User user;
 	
+	@Id
 	@OneToOne
 	private Annonce annonce;
 	
