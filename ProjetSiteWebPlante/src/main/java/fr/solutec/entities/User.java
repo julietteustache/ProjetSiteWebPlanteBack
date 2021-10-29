ae7686bd3bf2b2d57a22efde34852ad18789a4d9
julietteustache <juliette.eustache@icloud.com>
2021-10-29 09:23:58 +0200

    creation package entities

--- ProjetSiteWebPlante/src/main/java/fr/solutec/entities/User.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/entities/User.java
@@ -1,5 +1,13 @@
 package fr.solutec.entities;
 
 public class User {
+	private Long idUser;
+	private String nom;
+	private String prenom;
+	private String login;
+	private String mdp;
+	private String statue;
+	private Long score;
+	//private Adresse adresse;
 
 }
