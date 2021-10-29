3a9dd53dc8ebefd11ee716b138046df235d0f193
Rose-MarieG <rosemarie.gallon@gmail.com>
2021-10-29 10:12:05 +0200

    Merge branch 'master' of https://github.com/julietteustache/ProjetSiteWebPlanteBack

---
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/entities/EvenementUserConstraint.java
@@ -0,0 +1,15 @@
+package fr.solutec.entities;
+
+import java.io.Serializable;
+
+public class EvenementUserConstraint implements Serializable{
+	
+	private Long evenement;
+	
+	private Long user;
+	
+	public EvenementUserConstraint() {
+//création du couple de clé primaire		
+	}
+
+}
