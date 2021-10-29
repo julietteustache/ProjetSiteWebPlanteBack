f9b7282fd27ada6bb631c9f038cb517dc380d2b1
Arthur Cornet <coarthur@esic.fr>
2021-10-29 10:34:09 +0200

    classe commande

--- ProjetSiteWebPlante/src/main/java/fr/solutec/entities/Commande.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/entities/Commande.java
@@ -1,10 +1,6 @@
 package fr.solutec.entities;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
@@ -13,15 +9,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@IdClass(AnnonceUserConstraint.class)
 @NoArgsConstructor @AllArgsConstructor @Data
-public class Commande implements Serializable{
-	
-	@Id
+public class Commande {
 	@ManyToOne
 	private User user;
 	
-	@Id
 	@OneToOne
 	private Annonce annonce;
 	
