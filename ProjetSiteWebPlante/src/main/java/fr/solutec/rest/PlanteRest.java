88d598c29d399d0898f56869a99493569e584465
ZaherM7 <msaidie.zaher@yahoo.fr>
2021-10-29 13:54:38 +0200

    Ajouts de fonctions plantes

--- ProjetSiteWebPlante/src/main/java/fr/solutec/rest/PlanteRest.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/rest/PlanteRest.java
@@ -3,6 +3,8 @@ package fr.solutec.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Plante;
@@ -13,9 +15,15 @@ public class PlanteRest {
 	@Autowired
 	private PlanteRepository planteRepos;
 	
-	@GetMapping("plante")
-	public Iterable<Plante> rechercheNomPlantes(String espece){
+	@PostMapping("plante")
+	public Iterable<Plante> rechercheNomPlantes(@RequestBody String espece){
 		Iterable<Plante> p=planteRepos.findByEspece(espece);
+		return p;
+	}
+	
+	@GetMapping("plante")
+	public Iterable<Plante> getPlantes() {
+		Iterable<Plante> p=planteRepos.findAll();
 		return p;
 	}
 	
