591d9b2c43ecccc017a7638f0d5e9fc8ac8021ef
julietteustache <juliette.eustache@icloud.com>
2021-11-03 14:42:12 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/julietteustache/ProjetSiteWebPlante.git

--- ProjetSiteWebPlante/src/main/java/fr/solutec/rest/AnnonceRest.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/rest/AnnonceRest.java
@@ -1,14 +1,17 @@
 package fr.solutec.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Annonce;
-import fr.solutec.entities.User;
+
 import fr.solutec.repository.AnnonceRepository;
 @RestController @CrossOrigin("*")
 
@@ -21,10 +24,34 @@ public class AnnonceRest {
 		return annonceRepos.findAll();
 	}
 		
-	@PostMapping("/annonce") //
-	public Annonce saveUser(@RequestBody Annonce a) {
+	@PostMapping("/new_annonce") //
+	public Annonce saveAnnonce(@RequestBody Annonce a) {
 		return annonceRepos.save(a);
 	}
+	
+	
+	
+	@GetMapping("annonce_categorie/{categorie}") //avoir des données
+	public List<Annonce> getAnnonceByCategorie(@PathVariable String categorie){
+		return annonceRepos.findByPlanteCategorie(categorie);
+	}
+	
+	@GetMapping("annonce_souscategorie/{sousCategorie}") //avoir des données
+	public List<Annonce> getAnnonceBySousCategorie(@PathVariable String sousCategorie){
+		return annonceRepos.findByPlanteSousCategorie(sousCategorie);
+	}
+	
+	@GetMapping("annonce_espece/{espece}") //avoir des données
+	public List<Annonce> getAnnonceByEspece(@PathVariable String espece){
+		return annonceRepos.findByPlanteEspece(espece);
+	}
+	
+	@GetMapping("annonce_type/{type}") //avoir des données
+	public List<Annonce> getAnnonceByType(@PathVariable String type){
+		return annonceRepos.findByType(type);
+	}
+	
+	
 }
 	
 
