83610f819de4d4ca85e7757204b4fd8c57a5f375
ZaherM7 <msaidie.zaher@yahoo.fr>
2021-11-06 23:06:58 +0100

    Ajouts de fonctions pour les messages pour une tentative de messagerie

--- ProjetSiteWebPlante/src/main/java/fr/solutec/repository/MessageRepository.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/repository/MessageRepository.java
@@ -12,6 +12,18 @@ import fr.solutec.entities.User;
 
 public interface MessageRepository extends CrudRepository<Message, Long>{
     public Optional<Message>findByDestinataireAndExpediteur(User destinataire,User expediteur);
+    /* Je retire ça pour un test - Zaher
     public Stream<Message>findByExpediteur(User expediteur);
     public Stream<Message>findByDestinataire(User destinataire);
+    */
+    
+    public Iterable<Message> findByExpediteur(User expediteur);
+    public Iterable<Message> findByExpediteurIdUser(Long idUser);
+    
+    
+    public Iterable<Message> findByDestinataire(User destinataire);
+    public Iterable<Message> findByDestinataireIdUser(Long idUser);
+    
+    
+    public Iterable<Message> findByDestinataireIdUserAndExpediteurIdUser(Long idExpediteur, Long idDestinataire);
 }
--- ProjetSiteWebPlante/src/main/java/fr/solutec/rest/MessageRest.java
+++ ProjetSiteWebPlante/src/main/java/fr/solutec/rest/MessageRest.java
@@ -5,20 +5,41 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Message;
 import fr.solutec.entities.User;
 import fr.solutec.repository.MessageRepository;
 
+@RestController @CrossOrigin("*")
 public class MessageRest {
 	@Autowired
 	private MessageRepository messageRepos;
+	
+	/* Je retire ça pour un test - Zaher
 	@GetMapping("message")
 	public Stream <Message> getMess(@RequestBody User expediteur,@RequestBody User destinataire) {
 		return Stream.concat(messageRepos.findByExpediteur(expediteur),messageRepos.findByDestinataire(destinataire));
+	}
+	*/
+	
+	@GetMapping("messages/{idUser}/{idDestinataire}")
+	public Iterable<Message> getMessagesByDestinataire(@PathVariable Long idUser, @PathVariable Long idDestinataire) {
+		return messageRepos.findByDestinataireIdUserAndExpediteurIdUser(idUser, idDestinataire);
+	}
+	
+	@GetMapping("expediteur/{idUser}") //Tous les messages dont idUser est l'expéditeur
+	public Iterable<Message> getMsgExpediteur(@PathVariable Long idUser) {
+		return messageRepos.findByExpediteurIdUser(idUser);
+	}
+	
+	@GetMapping("destinataire/{idUser}")
+	public Iterable<Message> getMsgDestinataire(@PathVariable Long idUser) {
+		return messageRepos.findByDestinataireIdUser(idUser);
 	}
 
 }
